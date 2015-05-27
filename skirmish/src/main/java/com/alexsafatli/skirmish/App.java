883a17dafd45301fba2d894b80ec3947f7957fdf
Alex Safatli <safatli@cs.dal.ca>
2015-05-27 08:41:03 -0300

    More progress

--- skirmish/src/main/java/com/alexsafatli/skirmish/App.java
+++ skirmish/src/main/java/com/alexsafatli/skirmish/App.java
@@ -3,15 +3,6 @@ package com.alexsafatli.skirmish;
 import java.awt.Color;
 import java.awt.Font;
  
-import org.apache.pivot.collections.Map;
-import org.apache.pivot.wtk.Application;
-import org.apache.pivot.wtk.DesktopApplicationContext;
-import org.apache.pivot.wtk.Display;
-import org.apache.pivot.wtk.HorizontalAlignment;
-import org.apache.pivot.wtk.Label;
-import org.apache.pivot.wtk.VerticalAlignment;
-import org.apache.pivot.wtk.Window;
- 
 public class App implements Application {
     
     private Window window = null;
@@ -20,16 +11,9 @@ public class App implements Application {
     public void startup(Display display, Map<String, String> properties) {
         window = new Window();
  
-        Label label = new Label();
-        label.setText("Hello World!");
-        label.getStyles().put("font", new Font("Arial", Font.BOLD, 24));
-        label.getStyles().put("color", Color.RED);
-        label.getStyles().put("horizontalAlignment",
-            HorizontalAlignment.CENTER);
-        label.getStyles().put("verticalAlignment",
-            VerticalAlignment.CENTER);
+        TablePane pane = new TablePane();
  
-        window.setContent(label);
+        window.setContent(pane);
         window.setTitle("Hello World!");
         window.setMaximized(true);
  
